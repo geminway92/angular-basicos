ea4082e5259224c957a3a6a972f92f68768ed844
Gema Martín <geminway@live.com>
2022-02-22 13:00:13 +0100

    first commit

---
+++ src/app/contador/contador/contador.components.ts
@@ -0,0 +1,24 @@
+import {Component}  from '@angular/core';
+
+@Component({
+  selector: 'app-contador',
+  template: `
+  <h1>{{title}}</h1>
+  <h3>{{base}}</h3>
+  <button (click)="acumular(base)"> +{{base}}</button>
+  <span> {{numero}} </span>
+  <button (click)="acumular(-base)"> -{{base}}</button>
+  `
+
+})
+
+export class ContadorComponent {
+  title: string = 'Contador App';
+  numero: number =  10;
+  base: number = 5;
+
+  acumular( valor:number){
+    this.numero += valor;
+  }
+
+}
